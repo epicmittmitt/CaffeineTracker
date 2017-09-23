7e65ac2f1d501ecf3ec41b5617ca0382ba13e1e5
dotnoppler <justin.bi.1999@gmail.com>
2017-09-23 06:53:11 -0400

    one two

--- CaffeineTracker/Resources/Resource.Designer.cs
+++ CaffeineTracker/Resources/Resource.Designer.cs
@@ -48,7 +48,25 @@ namespace CaffeineTracker
 			public const int backButton = 2130968576;
 			
 			// aapt resource value: 0x7f040002
-			public const int myButton = 2130968578;
+			public const int linearLayout1 = 2130968578;
+			
+			// aapt resource value: 0x7f040008
+			public const int myButton = 2130968584;
+			
+			// aapt resource value: 0x7f040003
+			public const int textView1 = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int textView2 = 2130968580;
+			
+			// aapt resource value: 0x7f040006
+			public const int textView3 = 2130968582;
+			
+			// aapt resource value: 0x7f040005
+			public const int textView4 = 2130968581;
+			
+			// aapt resource value: 0x7f040007
+			public const int textView5 = 2130968583;
 			
 			// aapt resource value: 0x7f040001
 			public const int textureView1 = 2130968577;
