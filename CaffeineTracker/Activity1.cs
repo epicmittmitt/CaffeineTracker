5d2f3735c1cda6801fd67acdd4d94b8bb330f002
dotnoppler <justin.bi.1999@gmail.com>
2017-09-23 02:20:51 -0400

    ~

---
+++ CaffeineTracker/Activity1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Security;
+using Camera = Android.Hardware.Camera;
+
+namespace CaffeineTracker
+{
+    [Activity(Label = "Activity1")]
+    public class Activity1 : Activity,TextureView.ISurfaceTextureListener
+    {
+        private Camera _camera;
+        private TextureView _textureView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            _textureView = new TextureView(this);
+            _textureView.SurfaceTextureListener = this;
+
+            SetContentView(_textureView);
+        }
+
+        public void OnSurfaceTextureAvailable(Android.Graphics.SurfaceTexture surface, int w, int h)
+        {
+            _camera = Camera.Open();
+            
+            _textureView.LayoutParameters = new FrameLayout.LayoutParams(w, h);
+
+            try
+            {
+                _camera.SetPreviewTexture(surface);
+                _camera.StartPreview();
+            }
+            catch (Java.IO.IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+            
+        }
+
+        public bool OnSurfaceTextureDestroyed(Android.Graphics.SurfaceTexture surface)
+        {
+            _camera.StopPreview();
+            _camera.Release();
+
+            return true;
+
+        }
+
+        public void OnSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height)
+        {
+          
+        }
+
+        public void OnSurfaceTextureUpdated(SurfaceTexture surface)
+        {
+           
+        }
+    }
+}
--- CaffeineTracker/MainActivity.cs
+++ CaffeineTracker/MainActivity.cs
@@ -7,10 +7,12 @@ namespace CaffeineTracker {
 	public class MainActivity : Activity {
 		protected override void OnCreate(Bundle savedInstanceState) {
 			base.OnCreate(savedInstanceState);
+			
 
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 		}
+		 
 	}
 }
 
